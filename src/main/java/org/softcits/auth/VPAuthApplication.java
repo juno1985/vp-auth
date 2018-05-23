0668b1bec9c5e8e46bf22cbc77b7b02370ce4a3f
juno1985 <wangzhen_tju@126.com>
2018-05-23 16:34:46 +0800

    mybatis generate

---
+++ src/main/java/org/softcits/auth/VPAuthApplication.java
@@ -0,0 +1,11 @@
+package org.softcits.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VPAuthApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(VPAuthApplication.class, args);
+	}
+}
